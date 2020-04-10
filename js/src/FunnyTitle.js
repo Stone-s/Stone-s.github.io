765dff3d1f3ff2ac2fb0bf714ebbf71c2efd0b31
xinyu <sly990411@outlook.com>
2020-04-10 19:49:38 +0800

    Site updated: 2020-04-10 19:49:38

--- js/src/FunnyTitle.js
+++ js/src/FunnyTitle.js
@@ -1,14 +1,14 @@
-// 搞笑标题
+
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
          document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/favicon.ico");
          document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
