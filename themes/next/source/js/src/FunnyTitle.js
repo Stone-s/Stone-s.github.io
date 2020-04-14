21c65b4dce6903f6be22cc8f010430dfa122cc6c
Stone <sly990411@outlook.com>
2020-04-14 16:50:26 +0800

    update

--- themes/next/source/js/src/FunnyTitle.js
+++ themes/next/source/js/src/FunnyTitle.js
@@ -1,17 +1,17 @@
 
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/image/favicon.ico");
-        document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
-        clearTimeout(titleTime);
-    }
-    else {
-        $('[rel="icon"]').attr('href', "/image/favicon.ico");
-        document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
-});
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！ ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 你帅就回来！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
